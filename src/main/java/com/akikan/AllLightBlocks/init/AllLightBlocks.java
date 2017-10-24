3da4c3d3d56f709465464686a39aaf4277f2f6d2
AkikanAlumiloid <akikanalumiloid@LIVE.jp>
2017-10-24 23:55:25 +0900

    不要なファイルを削除

--- src/main/java/com/akikan/AllLightBlocks/init/AllLightBlocks.java
+++ src/main/java/com/akikan/AllLightBlocks/init/AllLightBlocks.java
@@ -1,6 +1,5 @@
 package com.akikan.AllLightBlocks.init;
 
-import com.akikan.AllLightBlocks.init.blocks.BlockStrongGlowstone;
 import com.akikan.AllLightBlocks.init.blocks.lumen16.*;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
@@ -11,7 +10,6 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class AllLightBlocks{
 
-	public static Block STRONG_GLOWSTONE;
 	public static Block BONE_LUMEN16;
 	public static Block BOOKSHELF_LUMEN16;
 	public static Block CLAY_LUMEN16;
@@ -35,7 +33,6 @@ public class AllLightBlocks{
 	public static Block STONEBRICK_LUMEN16;
 
 	public static void init(){
-		STRONG_GLOWSTONE = new BlockStrongGlowstone();
 		BONE_LUMEN16 = new BlockBone_Lumen16();
 		BOOKSHELF_LUMEN16 = new BlockBookshelf_Lumen16();
 		CLAY_LUMEN16 = new BlockClay_Lumen16();
@@ -61,7 +58,6 @@ public class AllLightBlocks{
 	}
 
 	public static void register(){
-		registerBlock(STRONG_GLOWSTONE);
 		registerBlock(BONE_LUMEN16);
 		registerBlock(BOOKSHELF_LUMEN16);
 		registerBlock(CLAY_LUMEN16);
@@ -93,7 +89,6 @@ public class AllLightBlocks{
 	}
 
 	public static void registerRender(){
-		registerRenders(STRONG_GLOWSTONE);
 		registerRenders(BONE_LUMEN16);
 		registerRenders(BOOKSHELF_LUMEN16);
 		registerRenders(CLAY_LUMEN16);
